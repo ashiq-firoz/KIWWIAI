0ea94decbc7fe0a399797f9ce0480f933d2cf072
hyperbala <balagopal123@gmail.com>
2024-06-23 16:51:43 +0530

    added chat page(not complete)

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "@/components/Provider";
+import { ThemeProvider } from "@/components/theme-provider"
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +19,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      
       <Provider session={undefined}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
         <body className={inter.className}>{children}</body>
+        </ThemeProvider>
       </Provider>
     </html>
   );
---
+++ client/components/Chat.jsx
@@ -0,0 +1,70 @@
+"use client"
+
+import { useState, useEffect } from 'react';
+import {MdDarkMode , MdLightMode } from "react-icons/md";
+import { FaArrowUpLong } from "react-icons/fa6";
+
+import ChatNav from "./ChatNav";
+
+
+export default function Chat() {
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState('');
+  const [darkMode, setDarkMode] = useState(true);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (input.trim() !== '') {
+      setMessages([...messages, { text: input, sender: 'User' }]);
+      setInput('');
+    }
+  };
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
+
+  return (
+    <div className='flex h-screen dark:bg-gray-900 bg-gray-100'>
+      <ChatNav/>
+      <div className="m-auto w-full max-w-xl bg-white dark:bg-gray-900 rounded-lg shadow-lg">
+        
+
+        <div className="flex flex-col h-96 overflow-y-auto p-4 dark:bg-gray-800 dark:text-white">
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`mb-2 p-2  rounded-2xl ${
+                message.sender === 'User'
+                  ? 'bg-gray-200 dark:bg-gray-600 text-black dark:text-white self-end'
+                  : 'bg-gray-200 dark:bg-gray-700 self-start'
+              }`}
+            >
+              {message.text}
+            </div>
+          ))}
+        </div>
+        <form className="flex p-4 border-t border-gray-200 dark:border-gray-700" onSubmit={sendMessage}>
+          <input
+            type="text"
+            className="flex-1 px-4 py-2 border bg-white text-black dark:text-white dark:bg-gray-800 border-gray-300 dark:border-gray-600 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            value={input}
+            placeholder='Message Kiwwi'
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button
+            type="submit"
+            disabled={input.title}
+            className="px-3 py-2  text-white dark:text-black dark:bg-white bg-black   rounded-full"
+          >
+            <FaArrowUpLong />
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}

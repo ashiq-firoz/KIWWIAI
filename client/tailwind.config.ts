378faa79e09a30ba4587ab09aaedfd250f05cb11
hyperbala <balagopal123@gmail.com>
2024-07-01 19:25:14 +0530

    changed button in home page and removed hydration errror

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -29,6 +29,14 @@ const config: Config = {
             backgroundPosition: "350% 50%, 350% 50%",
           },
         },
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
       },
 
     },
